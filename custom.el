08c8eead5a319d2f1519094c7b148adb98323a93
Imre Horvath <imi.horvath@gmail.com>
2014-11-12 19:41:26 +0100

    Change scheme program name

--- custom.el
+++ custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(js-indent-level 2)
- '(scheme-program-name "stk"))
+ '(scheme-program-name "guile"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
