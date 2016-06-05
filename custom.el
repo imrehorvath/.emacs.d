093186e14e54a5f78209c372343e0184ca210301
Imre Horvath <imi.horvath@gmail.com>
2016-06-05 08:41:22 +0200

    Use Stklos scheme

--- custom.el
+++ custom.el
@@ -6,7 +6,7 @@
  '(js-indent-level 2)
  '(projectile-find-dir-includes-top-level t)
  '(projectile-switch-project-action (quote projectile-find-dir))
- '(scheme-program-name "guile")
+ '(scheme-program-name "stklos")
  '(send-mail-function (quote mailclient-send-it)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
