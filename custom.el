0cc36970031b134e20e769bbe1aafd4ed2b05e06
Imre Horvath <imi.horvath@gmail.com>
2019-06-17 11:31:15 +0200

    Configure elpy to use python3

--- custom.el
+++ custom.el
@@ -3,9 +3,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(elpy-rpc-python-command "python3")
  '(package-selected-packages
    (quote
-    (flycheck elpy yaml-mode flx restclient paredit flx-ido exec-path-from-shell company))))
+    (flycheck elpy yaml-mode flx restclient paredit flx-ido exec-path-from-shell company)))
+ '(python-shell-interpreter "python3"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
