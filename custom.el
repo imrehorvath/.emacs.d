3d9fc07dff8d6c866092c7ccb9077ed0c2005b7e
Imre Horvath <imi.horvath@gmail.com>
2019-10-11 14:40:28 +0200

    Change theme

--- custom.el
+++ custom.el
@@ -3,7 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (tango-dark)))
+ '(custom-enabled-themes (quote (wombat)))
  '(elpy-rpc-python-command "python3")
  '(flycheck-python-flake8-executable "python3")
  '(flycheck-python-pylint-executable "python3")
