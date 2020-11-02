87bb8dbc80ac79f82f2e005e9f3bb44f161144da
Imre Horvath <imi.horvath@gmail.com>
2020-11-02 09:24:39 +0100

    Improve font-lock definitions

--- lisp/logo-mode.el
+++ lisp/logo-mode.el
@@ -1,14 +1,16 @@
 
 (defvar logo-keywords
-  '("stop" "op" "output"))
+  '("stop" "op" "output" "throw" "catch"))
 
 ;;;###autoload
 (define-derived-mode logo-mode fundamental-mode "logo"
   "Major mode for editing Logo source code"
   (setq font-lock-defaults `(((";+.*$" . font-lock-comment-face)
-			      ("\"[^ \t\n]*" . font-lock-constant-face)
-			      (":[a-zA-Z0-9.]+" . font-lock-variable-name-face)
-			      ("^[ \t]*\\(to\\|\\.macro\\|end\\)" . font-lock-keyword-face)
+			      ("\"\\(\\w\\|[.,`\"_?@]\\)+" . font-lock-constant-face)
+			      ("\"[\\]." . font-lock-constant-face)
+			      (":\\(\\w\\|[.,`_?@]\\)+" . font-lock-variable-name-face)
+			      ("^[ \t]*\\(to\\|\\.macro\\)[ \t]+" . font-lock-keyword-face)
+			      ("^[ \t]*end[ \t\n]+" . font-lock-keyword-face)
 			      ( ,(regexp-opt logo-keywords 'words) . font-lock-keyword-face))
 			     t))
   (setq comment-start ";;")
