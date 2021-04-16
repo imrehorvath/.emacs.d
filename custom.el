cedb664cb15633fd88fe84bf750a46207a8f0565
Imre Horvath <imi.horvath@gmail.com>
2021-04-16 20:45:56 +0200

    Remove custom enabled themes

--- custom.el
+++ custom.el
@@ -3,7 +3,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes '(wombat))
  '(explicit-shell-file-name "/bin/zsh")
  '(package-selected-packages
    '(magit ztree json-mode flx paredit flx-ido exec-path-from-shell company)))
