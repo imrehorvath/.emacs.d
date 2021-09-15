caea87fb077b0d5f121edc4cc430a6c42b5a7383
Imre Horvath <imi.horvath@gmail.com>
2021-09-15 16:01:04 +0200

    Improve sloppy implementation

--- lisp/system-dark-mode.el
+++ lisp/system-dark-mode.el
@@ -3,7 +3,8 @@
 (defvar preferred-dark-theme nil)
 (defvar preferred-light-theme nil)
 
-(defvar dark-or-light-state nil)
+(defvar dark-mode-state-initialized nil)
+(defvar emacs-in-dark-mode nil)
 
 (defun system-dark-mode-enabled-p ()
   "Checks if the system is in dark mode.
@@ -12,32 +13,27 @@ When the system-specific check for dark mode has been implemented by
 this function, returns the result of this check.
 Returns nil when the check is not implemented."
   (cond ((eq system-type 'darwin)
-	 (= 0 (shell-command "defaults read -g AppleInterfaceStyle")))
+	 (string= "Dark"
+		  (string-trim (shell-command-to-string
+				"defaults read -g AppleInterfaceStyle"))))
 	;; TODO: Add check for dark mode on other systems too!
 	(t nil)))
 
 (defun match-system-dark-mode ()
-  "Check if the system is in dark mode, and set themes accordingly."
+  "Match the system dark mode settings with preferred emacs themes.
+
+Enables/disables themes in emacs to match the system-wide dark mode settings."
   (interactive)
-  (unless (and preferred-dark-theme
-	       preferred-light-theme)
-    (error "preferred-dark-theme and preferred-light-theme variables needs to be set prior calling function match-system-dark-mode"))
-  (cond ((eq dark-or-light-state 'light)
-	 (when (system-dark-mode-enabled-p)
-	   (disable-theme preferred-light-theme)
-	   (load-theme preferred-dark-theme t)
-	   (setq dark-or-light-state 'dark)))
-	((eq dark-or-light-state 'dark)
-	 (unless (system-dark-mode-enabled-p)
-	   (disable-theme preferred-dark-theme)
-	   (load-theme preferred-light-theme t)
-	   (setq dark-or-light-state 'light)))
-	((not dark-or-light-state)
-	 (if (system-dark-mode-enabled-p)
-	     (progn (load-theme preferred-dark-theme t)
-		    (setq dark-or-light-state 'dark))
-	   (load-theme preferred-light-theme t)
-	   (setq dark-or-light-state 'light)))
-	(t (error "dark-or-light-state is invalid: %s" dark-or-light-state))))
+  (let ((system-in-dark-mode (system-dark-mode-enabled-p)))
+    (when (or (not dark-mode-state-initialized)
+	      (not (eq emacs-in-dark-mode system-in-dark-mode)))
+      (let ((theme-to-use (if system-in-dark-mode
+			      preferred-dark-theme
+			    preferred-light-theme)))
+	(when theme-to-use
+	  (dolist (theme custom-enabled-themes) (disable-theme theme))
+	  (load-theme theme-to-use t)))
+      (setq emacs-in-dark-mode system-in-dark-mode
+	    dark-mode-state-initialized t))))
 
 (provide 'system-dark-mode)
